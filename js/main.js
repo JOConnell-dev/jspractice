509561f695e59f964c5c596320ab567554656188
Jeffrey OConnell <jeff.oconnell.dev@gmail.com>
2021-01-04 13:16:58 -0800

    typeOf

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 console.log("Test");
+
+// CREATE AN ARRAY//
+var fruits = ['apples', 'oranges','bananas'];
+var scores = [98,33,22,12,43];
+// CREATE AN OBJECT//
+var numbersObj = {
+    count : 4,
+    remaining: 7
+};
+
+var dogsObj = {
+    name: 'Gunner',
+    color: 'Brown',
+    age: 7
+};
+
+var dogsObj = {
+    name: 'Pepper',
+    color: 'Brown',
+    age: 8
+};
+
+var studentsObj = {
+    name: 'Mary',
+    grade: 'A'
+}
+
+// typeOf //
+console.log('type of an array', typeof [3,4,5]);
+console.log('type of 8', typeof 8);
+ 
